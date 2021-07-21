2a322bea18f753ea64d6ff2c426c485b9ccc548d
ShevDenny <sdenny904@gmail.com>
2021-07-21 10:25:08 -0400

    commit tree ce851a4a4b5e4b8dec76c358fa4a38e6f4fd09b7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,37 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
+
+
+
+
+    # validate :clickbait
+
+    # clickbait_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+    # def clickbait
+    #     # unless title.match? (["Won't Believe", "Secret", "Top [number]", "Guess"])
+    #     #     error.add(:title, "Not clickbait-y")
+    #     # end
+    #     if clickbait_titles.none? { |title| title.match title}
+    #         error.add(:title, "Not clickbait-y")
+    #     end
+    # end
